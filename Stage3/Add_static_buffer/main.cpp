c5c8b2e52e7f5e9c2383f854111ebc1e795e8282
K4nishkk <115213375+K4nishkk@users.noreply.github.com>
2024-08-24 16:38:23 +0530

    Create main.cpp

---
+++ Stage3/Add_static_buffer/main.cpp
@@ -0,0 +1,64 @@
+// We need to get all the attribute catalog blocks
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+#include "string.h"
+
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+  StaticBuffer buffer;
+
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i{}; i < relCatHeader.numEntries; i++) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("\nRelation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j{}; j < attrCatHeader.numEntries; j++) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+
+    RecBuffer newBuffer = attrCatBuffer;
+    HeadInfo newHeader = attrCatHeader;
+
+    while (newHeader.rblock != -1) {
+      RecBuffer tempBuffer(newHeader.rblock);
+      newBuffer = tempBuffer;
+      newBuffer.getHeader(&newHeader);
+      for (int j{}; j < newHeader.numEntries; j++) {
+
+        // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        newBuffer.getRecord(attrCatRecord, j);
+
+        if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0) {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
+      }
+    }
+  }
+
+  return 0;
+} 
